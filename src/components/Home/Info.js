de1d53e1c8a4a8e8a0134f6e7a5c1553cacf0e32
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-21 12:19:15 +0200

    up to info and title components

---
+++ src/components/Home/Info.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "gatsby";
+import Title from "../Globals/Title";
+
+export default function Info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="our story" />
+        <div className="row">
+          <div className="col-10 col-sm mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reiciendis, magni. Similique in qui, magnam aperiam nam earum sit
+              mollitia eos excepturi sunt illum, dolores obcaecati quaerat vel
+              ex error facilis. Ullam totam, doloribus modi ipsam voluptatibus
+              ab repellat tempora est culpa mollitia sapiente officia fugiat
+              quod! Sapiente commodi quia dolore.
+            </p>
+            <Link to="/about">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,32 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+import Info from "../components/Home/Info";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      // styleClass="default-background"
+    />
+    <Info />
   </Layout>
-)
+);
 
-export default IndexPage
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+export default IndexPage;
