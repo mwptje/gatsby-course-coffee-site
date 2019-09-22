69078cd3a54660b84ca4da301b5ad753b0c1d511
mvdvalk <46578571+mwptje@users.noreply.github.com>
2019-09-22 11:00:36 +0200

    added menu items

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -15,6 +16,7 @@ const IndexPage = ({ data }) => (
       // styleClass="default-background"
     />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 );
 
@@ -27,6 +29,25 @@ export const query = graphql`
         }
       }
     }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
   }
 `;
+
 export default IndexPage;
